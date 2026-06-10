0c802efed2221b1dbfd7fc8c02ae7fc04cbd2901
Rhas92 <ramonaller@gmail.com>
2026-06-11 01:11:58 +0200

    feat: add Ticket CRUD with service, controller, repository and exception

---
+++ src/main/java/com/example/tickets_api/service/TicketService.java
@@ -0,0 +1,48 @@
+package com.example.tickets_api.service;
+
+import com.example.tickets_api.exceptions.TicketNotFoundException;
+import com.example.tickets_api.model.Priority;
+import com.example.tickets_api.model.Status;
+import com.example.tickets_api.model.Ticket;
+import com.example.tickets_api.repository.TicketRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@Service
+public class TicketService {
+    private final TicketRepository ticketRepository;
+
+    public TicketService(TicketRepository ticketRepository) {
+        this.ticketRepository = ticketRepository;
+    }
+
+    public List<Ticket> getTickets() {
+        return ticketRepository.findAll();
+    }
+
+    public Ticket getTicketById(String id) {
+        return ticketRepository.findById(id)
+                .orElseThrow(() -> new TicketNotFoundException(id));
+    }
+    public List<Ticket> getTicketsByStatus(Status status) {
+        return ticketRepository.findByStatus(status);
+    }
+    public List<Ticket> getTicketsByPriority(Priority priority) {
+        return ticketRepository.findByPriority(priority);
+    }
+    public Ticket createTicket(Ticket ticket) {
+        return ticketRepository.save(ticket);
+    }
+
+    public Ticket updateTicket(Ticket ticket, String id) {
+        ticket.setId(id);
+        return ticketRepository.save(ticket);
+    }
+
+    public String deleteTicket(String id) {
+        ticketRepository.deleteById(id);
+        return "Ticket " + id + " deleted";
+    }
+}
